a7528aecd59279125e5bc3e87f7ee0f20cbbf757
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2023-12-10 21:20:03 +0100

    Computed.when

--- example/main.dart
+++ example/main.dart
@@ -30,27 +30,24 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Computed Flutter Demo'),
+      home: MyHomePage(title: 'Computed Flutter Demo'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
   final String title;
+
+  final Computed<BuiltList<int>> list = Computed(() {
+    final anyNegative = source.use.any((element) => element < 0);
+    final maybeReversed =
+        anyNegative ? source.use.reversed.toBuiltList() : source.use;
+    return maybeReversed.rebuild((p0) => p0.add(0));
+  });
 
   @override
   Widget build(BuildContext context) {
-    final anyNegative =
-        Computed(() => source.use.any((element) => element < 0));
-
-    final maybeReversed = Computed(
-        () => anyNegative.use ? source.use.reversed.toBuiltList() : source.use);
-
-    final append0 = Computed(() {
-      return maybeReversed.use.rebuild((p0) => p0.add(0));
-    });
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -60,13 +57,13 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ValueListenableBuilder(
-              valueListenable: append0.asValueListenable(<int>[].build()),
-              builder: (context, BuiltList<int> value, child) => Text(
-                value.toString(),
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            )
+            list.when(
+                context,
+                (value) => Text(
+                      value.toString(),
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                noValue: () => const Text('no value'))
           ],
         ),
       ),
--- lib/computed_flutter.dart
+++ lib/computed_flutter.dart
@@ -4,13 +4,99 @@ import 'package:computed/computed.dart';
 import 'package:computed/src/computed.dart';
 import 'package:computed_flutter/src/listenable_extension.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 import 'src/value_listenable_extension.dart';
 import 'src/computed_value_listenable.dart';
 
+class _ComputedMetadata<T> {
+  bool? _lastWasValue;
+  T? _lastValue;
+  Object? _lastError;
+}
+
+class _ComputedFlutterCtx {
+  static final _computations = <Computed, _ComputedMetadata>{};
+  static final _listeners =
+      <BuildContext, Map<Computed, ComputedSubscription>>{};
+  static var _postFrameCallbackScheduled = false;
+}
+
+void _postFrameCallback(Duration _) {
+  // TODO: Also garbage collect individual listeners, in case a widget stop listening a computation from one frame to another
+  // "Garbage-collect" unmounted BuildContexts
+  for (var kv in _ComputedFlutterCtx._listeners.entries) {
+    if (kv.key.mounted == false) {
+      for (var sub in kv.value.values) {
+        sub.cancel();
+      }
+      _ComputedFlutterCtx._listeners.remove(kv.key);
+    }
+  }
+}
+
 extension ComputedAsValueListenableExtension<T> on Computed<T> {
   ValueListenable<T> asValueListenable(T initial) {
     return ComputedValueListenable(this, initial);
+  }
+
+  Widget when(BuildContext context, Widget Function(T) onValue,
+      {required Widget Function() noValue, Widget Function(Object)? error}) {
+    var metadata = _ComputedFlutterCtx._computations[this];
+    if (metadata == null) {
+      metadata = _ComputedMetadata();
+      _ComputedFlutterCtx._computations[this] = metadata;
+    }
+    listenerCommon() {
+      assert(
+          SchedulerBinding.instance.schedulerPhase !=
+              SchedulerPhase.persistentCallbacks,
+          'Do not mutate state (by setting the value of the ValueNotifier '
+          'that you are subscribed to) during a `build` method. If you need '
+          'to schedule a value update after `build` has completed, use '
+          '`SchedulerBinding.instance.scheduleTask(updateTask, Priority.idle)`, '
+          '`SchedulerBinding.addPostFrameCallback(updateTask)`, '
+          'or similar.');
+      // Mark the element as needing to be rebuilt
+      (context as Element).markNeedsBuild();
+    }
+
+    valueListener(T event) {
+      listenerCommon();
+      metadata!._lastWasValue = true;
+      metadata._lastValue = event;
+    }
+
+    errorListener(Object error) {
+      listenerCommon();
+      metadata!._lastWasValue = false;
+      metadata._lastError = error;
+    }
+
+    _ComputedFlutterCtx._listeners
+        .putIfAbsent(context, () => <Computed, ComputedSubscription>{})
+        .putIfAbsent(this, () => listen(valueListener, errorListener));
+
+    if (!_ComputedFlutterCtx._postFrameCallbackScheduled) {
+      _ComputedFlutterCtx._postFrameCallbackScheduled = true;
+      SchedulerBinding.instance.addPostFrameCallback((_) {
+        _ComputedFlutterCtx._postFrameCallbackScheduled = false;
+        SchedulerBinding.instance.addPostFrameCallback(_postFrameCallback);
+      });
+    }
+
+    if (metadata._lastWasValue == null) {
+      return noValue();
+    } else if (metadata._lastWasValue == true) {
+      return onValue(metadata._lastValue);
+    } else {
+      if (error != null) {
+        return error(metadata._lastError!);
+      } else {
+        throw metadata._lastError!;
+      }
+    }
   }
 }
 
