0a012854982333f402b3eb4ed08e864b74ee7cb9
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2023-12-16 22:40:15 +0100

    Revert to the counter app example   But using ComputedWidget instead of StatefulWidget

--- example/main.dart
+++ example/main.dart
@@ -1,21 +1,10 @@
-import 'package:built_collection/built_collection.dart';
 import 'package:computed_flutter/computed_flutter.dart';
 
 import 'package:flutter/material.dart';
 
-final source = ValueNotifier(<int>[].toBuiltList());
+final count = ValueNotifier(0);
 
 void main() {
-  () async {
-    source.value = [1, 2, -3, 4].toBuiltList();
-    await Future.delayed(const Duration(seconds: 3));
-    source.value = [1, 2, -3, -4].toBuiltList();
-    await Future.delayed(const Duration(seconds: 3));
-    source.value = [4, 5, 6].toBuiltList();
-    await Future.delayed(const Duration(seconds: 3));
-    source.value = [4, 5, 6].toBuiltList();
-  }();
-
   runApp(const MyApp());
 }
 
@@ -30,39 +19,38 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: 'Computed Flutter Demo'),
+      home: const MyHomePage(),
     );
   }
 }
 
+// Note that unlike the vanilla counter app, this widget is stateless
 class MyHomePage extends ComputedWidget {
-  MyHomePage({super.key, required this.title});
-  final String title;
-
-  final Computed<BuiltList<int>> list = $(() {
-    final anyNegative = source.use.any((element) => element < 0);
-    final maybeReversed =
-        anyNegative ? source.use.reversed.toBuiltList() : source.use;
-    return maybeReversed.rebuild((p0) => p0.add(0));
-  });
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: const Text('Computed Flutter Demo'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              list.use.toString(),
-              style: Theme.of(context).textTheme.headlineMedium,
-            )
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text('${count.use}',
+                style: Theme.of(context).textTheme.headlineMedium)
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => count.value++,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
